6160300f8fa4cff72d68097485d446157d8f9da3
Felix Bytow <felix@staffbase.com>
2018-11-29 17:03:05 +0100

    :heavy_plus_sign: added FiraCode and incbin as dependencies

---
+++ game/resources.hxx
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2018, Felix Bytow <drako@drako.guru>
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef OVENBRICK_RESOURCES_HXX
+#define OVENBRICK_RESOURCES_HXX
+
+#include <incbin.h>
+
+INCBIN_EXTERN(FiraCode);
+
+#endif // OVENBRICK_RESOURCES_HXX
